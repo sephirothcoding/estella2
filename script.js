17813c4c5520fc535a7b9f0ab6da7ebc99903bd3
Sephiroth <78663409+sephirothcoding@users.noreply.github.com>
2022-11-24 11:01:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-if ( navigator.platform.indexOf('Win') != -1 ) {
-  window.document.getElementById("wrapper").setAttribute("class", "windows");
-} else if ( navigator.platform.indexOf('Mac') != -1 ) {
-  window.document.getElementById("wrapper").setAttribute("class", "mac");
-}
+
 
 
 // You can change global variables here:
